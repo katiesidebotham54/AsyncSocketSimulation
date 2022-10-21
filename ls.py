5d68a77be7437e45e882cc3673b61572972298f2
Aadit Singh <aaditsingh8@gmail.com>
2022-10-20 23:27:26 -0400

    Update LS before the point of sending queries

--- ls.py
+++ ls.py
@@ -1,65 +1,61 @@
 import socket
 import sys
 
+# Command line arguments
 port = int(sys.argv[1])
 ts1_host = sys.argv[2]
-ts1_port = sys.argv[3]
+ts1_port = int(sys.argv[3])
 ts2_host = sys.argv[4]
-ts2_port = sys.argv[5]
+ts2_port = int(sys.argv[5])
 
 
 def main():
 
-    # Create client socket to communicate with ts1
+    # Create load balancing socket to communicate with client and servers
     try:
-        s1 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        print("[S]: Client socket 1 for TS1 created")
+        ls = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        print("[LS]: Client socket 1 for TS1 created")
     except socket.error as err:
-        print("[S]: Couldn't create socket due to {}".format(err))
+        print("[LS]: Couldn't create socket due to {}".format(err))
         exit()
-
-    # Create client socket to communicate with ts2
-    try:
-        s2 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        print("[S]: Client socket 2 for TS2 created")
-    except socket.error as err:
-        print("[S]: Couldn't create socket due to {}".format(err))
-        exit()
-
-    # Connect to TS1 host
-    ts1_binding = (ts1_host, ts1_port)
-    s1.connect(ts1_binding)
-
-    # Connect to TS2 host
-    ts2_binding = (ts2_host, ts2_port)
-    s2.connect(ts2_binding)
-
-    # Create socket to communicate with Client
-    try:
-        ss = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        print("[S]: Server socket for client created")
-    except socket.error as err:
-        print("[S]: Couldn't create socket due to {}".format(err))
 
     # Define the port on which you want to connect to the server
-    hostname = socket.gethostname()
-    localhost_addr = socket.gethostbyname(hostname)
-    server_binding = (localhost_addr, port)
-    ss.bind(server_binding)
-    print("[S]: Successful Socket bind")
-    ss.listen(1)
+    server_binding = ('', port)
+    ls.bind(server_binding)
+    ls.listen(1)
 
     # Accept connection from Client socket
-    csockid, addr = ss.accept()
-    print("[S]: Got a connection request from a client at {}".format(addr))
+    csockid, addr = ls.accept()
+    print("[C]: Got a connection request from a client at {}".format(addr))
 
     while True:
         # Receive data from the client
         client_data = csockid.recv(200).decode('utf-8')
         if not client_data:
             break
+
         print("[C]: Data received from client: {}".format(
             client_data))
+
+        try:
+            sent = ls.send(client_data)
+            total_sent += sent
+            client_data = client_data[sent:]
+            print('Sending data')
+        except e:
+            if e.errno != errno.EAGAIN:
+                print('Blocking with', len(data), 'remaining')
+                select.select([], [ss], [])  # This blocks until
+        
+        response = -1
+
+        # Send value if exists
+        if not response == -1:
+            csockid.send(response.encode('utf-8'))
+        else:
+            error_msg = client_data + ' - TIMED OUT'
+            csockid.send(error_msg.encode('utf-8'))
+
     ss.close()
 
 
